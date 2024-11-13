137f3d9a434a48e63cd84c6d0835a33bf7ff3540
jean carlos <jeancarlos3102@gmail.com>
2024-11-13 12:02:36 -0500

    feat:(useEffect): using cleanup avoids re-rendering the listener event

--- src/HookApp.tsx
+++ src/HookApp.tsx
@@ -1,13 +1,15 @@
 // import CounterComponent from './useState/CounterComponent';
+// import PokemonListComponent from "./useEffect/PokemonListComponent";
 
-import PokemonListComponent from "./useEffect/PokemonListComponent";
+import CleanupComponent from "./useEffect/CleanupComponent";
 
 
 export default function HookApp() {
 	return (
 		<>
 			{/* <CounterComponent /> */}
-			<PokemonListComponent />
+			{/* <PokemonListComponent /> */}
+			<CleanupComponent/>
 		</>
 	);
 }
---
+++ src/useEffect/CleanupComponent.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+export default function CleanupComponent() {
+	const [coords, setCoords] = useState({ x: 0, y: 0 });
+	const [input, setInput] = useState('');
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target;
+		setInput(value);
+	};
+
+	useEffect(() => {
+		const onMouseMove = ({ x, y }: MouseEvent) => {
+			setCoords({ x, y });
+		};
+
+		window.addEventListener('mousemove', onMouseMove);
+
+		return () => {
+			window.removeEventListener('mousemove', onMouseMove);
+		};
+	}, [input]);
+	return (
+		<>
+			<input
+				type="text"
+				onChange={handleChange}
+				value={input}
+			/>
+
+			{JSON.stringify(coords)}
+		</>
+	);
+}
