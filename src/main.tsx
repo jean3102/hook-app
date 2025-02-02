d98c6c272b08de5a815b0d8e147f7dda0c5b115c
jean carlos <jeancarlos3102@gmail.com>
2025-02-02 13:44:05 -0500

    refactor(main): chnage to HashRouter for touting

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { HashRouter, Route, Routes } from 'react-router';
 import { routes } from './routes';
 import { Container } from './layout/Container';
 
 createRoot(document.getElementById('root')!).render(
 	<Container>
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				{routes.map(({ path, element }, index) => (
 					<Route
@@ -16,6 +16,6 @@ createRoot(document.getElementById('root')!).render(
 					/>
 				))}
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	</Container>
 );
